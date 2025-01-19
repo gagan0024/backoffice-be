3ede2fbe4dd3eece37f63ed67011c8f8e6aaa402
Krishan Bhardwaj <krazzzy7@gmail.com>
2025-01-19 08:21:30 +0530

    final design get and update data

--- src/controllers/admin/FinalDesignController.ts
+++ src/controllers/admin/FinalDesignController.ts
@@ -10,57 +10,80 @@ import { processErrors } from '../../utils/errorProcessing';
 import { ResponseCodes } from '../../utils/constants';
 
 class FinalDesignController {
-    // static listAll = async (req: Request, res: Response, next: NextFunction) => {
-    //     // Get the Sub Service ID from the url
-    //     const {
-    //         location_id,
-    //         building_id,
-    //         sub_building_id,
-    //         level_id,
-    //         service_id,
-    //         sub_service_id,
-    //         action_id,
-    //     } = req.query; // Access query parameter
-    //     let finalDesigns = []
+    static listAll = async (req: Request, res: Response, next: NextFunction) => {
+        // Get parameters from the body
+        const {
+            location_id,
+            building_id,
+            sub_building_id,
+            level_id,
+            room_id,
+            service_id,
+            sub_service_id,
+            action_id,
+            calculation_type
+        } = req.body;
+        let finalDesign;
 
-    //     if (location_id || building_id || sub_building_id || level_id || service_id || sub_service_id || action_id) {
-    //         // Execute the query with action_id
-    //         finalDesigns = await FinalDesign.find({
-    //             location_id,
-    //             building_id,
-    //             sub_building_id,
-    //             level_id,
-    //             service_id,
-    //             sub_service_id,
-    //             action_id,
-    //         });
-    //     } else {
-    //         // Execute the query
-    //         finalDesigns = await FinalDesign.find();
-    //     }
+        try {
+            if (calculation_type === 'BUILDING') {
+                finalDesign = await BuildingCalculation.findOne({
+                    location_id,
+                    building_id,
+                    service_id,
+                    sub_service_id,
+                    action_id
+                } as IBuildingCalculation);
+            }
 
-    //     // Send the finalDesigns object
-    //     res.send({
-    //         status: ResponseCodes.FINAL_DESIGN_DATA_LIST.code,
-    //         message: ResponseCodes.FINAL_DESIGN_DATA_LIST.message,
-    //         data: finalDesigns
-    //     });
-    // };
+            if (calculation_type === 'SUB_BUILDING') {
+                finalDesign = await SubBuildingCalculation.findOne({
+                    location_id,
+                    building_id,
+                    sub_building_id,
+                    service_id,
+                    sub_service_id,
+                    action_id,
+                } as ISubBuildingCalculation);
+            }
 
-    // static getOneById = async (req: Request, res: Response, next: NextFunction) => {
-    //     // Get the ID from the url
-    //     const id: string = req.params.id;
+            if (calculation_type === 'LEVEL') {
+                finalDesign = await LevelCalculation.findOne({
+                    location_id,
+                    building_id,
+                    sub_building_id,
+                    level_id,
+                    service_id,
+                    sub_service_id,
+                    action_id,
+                } as ILevelCalculation);
+            }
 
-    //     // Mongoose automatically casts the id to ObjectID
-    //     const finalDesign = await FinalDesign.findById(id);
-    //     if (!finalDesign) throw new NotFoundError(`Final Design with ID ${id} not found`);
+            if (calculation_type === 'ROOM') {
+                finalDesign = await RoomCalculation.findOne({
+                    location_id,
+                    building_id,
+                    sub_building_id,
+                    level_id,
+                    room_id,
+                    service_id,
+                    sub_service_id,
+                    action_id,
+                } as IRoomCalculation);
+            }
+        } catch (e: any) {
+            console.error(e);
+            const error = e as Error.ValidationError;
+            throw new ClientError(processErrors(error));
+        }
 
-    //     res.send({
-    //         status: ResponseCodes.FINAL_DESIGN_DATA_DETAILS.code,
-    //         message: ResponseCodes.FINAL_DESIGN_DATA_DETAILS.message,
-    //         data: finalDesign?.toJSON()
-    //     });
-    // };
+        // If all ok, send response
+        res.send({
+            status: ResponseCodes.FINAL_DESIGN_DATA_DETAILS.code,
+            message: ResponseCodes.FINAL_DESIGN_DATA_DETAILS.message,
+            data: finalDesign
+        });
+    };
 
     static newFinalDesign = async (req: Request, res: Response, next: NextFunction) => {
         // Get parameters from the body
@@ -141,67 +164,44 @@ class FinalDesignController {
         });
     };
 
-    // static editFinalDesign = async (req: Request, res: Response, next: NextFunction) => {
-    //     // Get the ID from the url
-    //     const id = req.params.id;
+    static editFinalDesign = async (req: Request, res: Response, next: NextFunction) => {
+        // Get the ID from the url
+        const id = req.params.id;
 
-    //     // Get values from the body
-    //     const {
-    //         location_id,
-    //         building_id,
-    //         sub_building_id,
-    //         level_id,
-    //         service_id,
-    //         sub_service_id,
-    //         action_id,
-    //         action_data
-    //     } = req.body;
+        // Get values from the body
+        const { calculation_type } = req.body;
 
-    //     // Mongoose automatically casts the id to ObjectID
-    //     const finalDesign = await FinalDesign.findById(id);
-    //     if (!finalDesign) throw new NotFoundError(`Final Design with ID ${id} not found`);
+        let finalDesign;
 
-    //     // Edit the properties
-    //     finalDesign.location_id = location_id
-    //     finalDesign.building_id = building_id
-    //     finalDesign.sub_building_id = sub_building_id
-    //     finalDesign.level_id = level_id
-    //     finalDesign.service_id = service_id
-    //     finalDesign.sub_service_id = sub_service_id
-    //     finalDesign.action_id = action_id
-    //     finalDesign.action_data = action_data
+        try {
+            if (calculation_type === 'BUILDING') {
+                finalDesign = await BuildingCalculation.updateOne({ _id: id }, { $set: req.body });
+            }
 
-    //     // Save and catch all validation errors
-    //     try {
-    //         await finalDesign.save();
-    //     } catch (e) {
-    //         const error = e as Error.ValidationError;
-    //         throw new ClientError(processErrors(error));
-    //     }
+            if (calculation_type === 'SUB_BUILDING') {
+                finalDesign = await SubBuildingCalculation.updateOne({ _id: id }, { $set: req.body });
+            }
 
-    //     res.send({
-    //         status: ResponseCodes.FINAL_DESIGN_DATA_UPDATED.code,
-    //         message: ResponseCodes.FINAL_DESIGN_DATA_UPDATED.message,
-    //         data: finalDesign.toJSON()
-    //     });
-    // };
+            if (calculation_type === 'LEVEL') {
+                finalDesign = await LevelCalculation.updateOne({ _id: id }, { $set: req.body });
+            }
 
-    // static deleteFinalDesign = async (req: Request, res: Response, next: NextFunction) => {
-    //     // Get the ID from the url
-    //     const id = req.params.id;
+            if (calculation_type === 'ROOM') {
+                finalDesign = await RoomCalculation.updateOne({ _id: id }, { $set: req.body });
+            }
+        } catch (e: any) {
+            console.error(e);
+            const error = e as Error.ValidationError;
+            throw new ClientError(processErrors(error));
+        }
 
-    //     // Mongoose automatically casts the id to ObjectID
-    //     const finalDesign = await FinalDesign.findById(id);
-    //     if (!finalDesign) throw new NotFoundError(`Final Design with ID ${id} not found`);
-
-    //     await finalDesign.delete();
-
-    //     // After all send response
-    //     res.send({
-    //         status: ResponseCodes.FINAL_DESIGN_DATA_DELETED.code,
-    //         message: ResponseCodes.FINAL_DESIGN_DATA_DELETED.message
-    //     });
-    // };
+        // If all ok, send response
+        res.send({
+            status: ResponseCodes.FINAL_DESIGN_DATA_UPDATED.code,
+            message: ResponseCodes.FINAL_DESIGN_DATA_UPDATED.message,
+            data: finalDesign?.modifiedCount
+        });
+    };
 }
 
 export default FinalDesignController;
